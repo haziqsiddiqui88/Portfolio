8e6fd333ca48cdae07988f50c04af9e0131e36a3
haziqsiddiqui8 <siddiquihaziq12@gmail.com>
2024-12-28 19:20:50 +0500

    Hero Changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,10 +21,10 @@ const Hero = () => {
             
             </div><div>
             <p className="text-[20px] mt-3 -ml-5 text-gray-400 italic mx-auto " data-aos='zoom-in-down'> 
-      I believe, "Exploring the future one algorithm at a time, because 
+      I believe, &quot;Exploring the future one algorithm at a time, because 
       <span className="font-bold underline ml-2 text-gray-600">AI</span><span className='ml-12'> and</span>
       <span className="font-bold underline  ml-1 text-gray-600">WEB DEVELOPMENT</span> 
-       <span className='ml-1'>is the language of tomorrow"</span>
+       <span className='ml-1'>is the language of tomorrow&rdquo;</span>
     </p></div>
           </div>
           <div className='-ml-[120px]'>
